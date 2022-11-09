51b1ef35ed6662acee232ad41d8a7f28bbbbd173
Salman <salman.naeem.112215@gmail.com>
2022-11-09 17:13:34 +0500

    Added Flexible in Catlog screen

--- lib/screen/catalog_screen.dart
+++ lib/screen/catalog_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import '../widget/payment.dart';
 
 class CatalogScreen extends StatefulWidget {
@@ -13,32 +11,39 @@ class CatalogScreen extends StatefulWidget {
 class _CatalogScreenState extends State<CatalogScreen> {
   @override
   Widget build(BuildContext context) {
-    final _mediaQuerry = MediaQuery.of(context);
-
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Column(
-        children: [
-          Container(
-            height: _mediaQuerry.size.height * 0.25,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40)),
-              color: Colors.white,
-            ),
-            width: double.infinity,
-            height: _mediaQuerry.size.height * 0.75,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Payment(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+                flex: 2,
+                fit: FlexFit.tight,
+                child: Container(
+                  width: double.infinity,
+                  child: Text('hi'),
+                )),
+            Flexible(
+              flex: 8,
+              fit: FlexFit.tight,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0),
+                      topRight: Radius.circular(40)),
+                  color: Colors.white,
+                ),
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Payment(),
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
